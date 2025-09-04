47f701278e077404e9c11b583430ee0ee4cce179
Eric <ecp@mgl.ca>
2025-09-04 18:06:55 +0000

    Add DMA read as OBI mgr. Integration in progress

--- sw/helloworld.c
+++ sw/helloworld.c
@@ -33,6 +33,14 @@ uint32_t isqrt(uint32_t n) {
 
 char receive_buff[16] = {0};
 
+// ascon test buffers
+char key[16] = { 0x90, 0xE4, 0x15, 0xD6, 0x42, 0xBF, 0xCD, 0x59, 0xF1, 0xFC, 0xCA, 0x19, 0x6B, 0x3B, 0xB3, 0x09 };
+char npub[16] = { 0x8C, 0xEE, 0x7C, 0xDD, 0x81, 0x83, 0xCA, 0x6A, 0xA2, 0xDC, 0x9B, 0x8B, 0x20, 0xA1, 0x6E, 0x8E };
+char ad[9] = { 0x2B, 0x0A, 0x5B, 0x7A, 0x81, 0xDE, 0x31, 0x73, 0xE2 };
+char pt[9] = { 0x32, 0x3B, 0x41, 0xEE, 0x00, 0xAE, 0x8A, 0x14, 0xAA };
+char ct[9] = { 0x3C, 0x71, 0xC7, 0xBA, 0xDE, 0x48, 0x01, 0x2E, 0x1D };
+char tag[16] = { 0xB2, 0x6E, 0x66, 0xA8, 0xA5, 0x5D, 0x6A, 0x93, 0x28, 0xD8, 0xD0, 0x5B, 0xC1, 0x67, 0x8A, 0x3E };
+
 int main() {
     uart_init(); // setup the uart peripheral
 
@@ -41,7 +49,39 @@ int main() {
     // wait until uart has finished sending
     uart_write_flush();
 
-    // uart loopback
+    // Create ASCON Instructions
+    printf("ASCON test\n");
+    long cmd[10];
+    cmd[0] = 1; // OP_ENCODE;
+    cmd[1] = (long)key;
+    cmd[2] = (long)npub;
+    cmd[3] = 9; // ad len
+    cmd[4] = (long)ad;
+    cmd[5] = 9; // msg len
+    cmd[6] = (long)pt;
+    cmd[7] = (long)ct;
+    cmd[8] = (long)tag;
+
+    //ascon[0] = cmd; // starte the code running
+
+    // Print out a string
+    printf( "CT = " );
+    for(uint8_t idx = 0; idx<9; idx++) {
+	printf( "%x ", ((char *)cmd[7])[idx] );
+    }
+    printf("\n");
+    printf( "CT long = %x\n", ((long *)cmd[7])[0] );
+    printf( "MAGIC = %x\n", *((long *)0x20000000) );
+    printf( "RDATA = %x\n", *((long *)0x20000004) );
+    printf( "dma run start\n");
+    uart_write_flush();
+    *((long *)0x20000004) = cmd[7]; 
+    printf( "dma run done\n");
+    printf( "RDATA = %x\n", *((long *)0x20000004) );
+    printf( "RDATA = %x\n", *((long *)0x20000004) );
+    uart_write_flush();
+    
+// uart loopback
     uart_loopback_enable();
     printf("internal msg\n");
     sleep_ms(1);
@@ -71,16 +111,16 @@ int main() {
     uart_write_flush();
 
     // doing some compute
-    uint32_t start = get_mcycle();
-    uint32_t res   = isqrt(1234567890UL);
-    uint32_t end   = get_mcycle();
-    printf("Result: 0x%x, Cycles: 0x%x\n", res, end - start);
-    uart_write_flush();
+    //uint32_t start = get_mcycle();
+    //uint32_t res   = isqrt(1234567890UL);
+    //uint32_t end   = get_mcycle();
+    //printf("Result: 0x%x, Cycles: 0x%x\n", res, end - start);
+    //uart_write_flush();
 
     // using the timer
     printf("Tick\n");
     sleep_ms(10);
-    printf("Tock\n");
+    printf("Tocking\n");
     uart_write_flush();
     return 1;
 }
