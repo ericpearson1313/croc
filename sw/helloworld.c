1f3c379a8151f2e79f789ca10b7c08af56bc6c72
Eric Pearson <ericpubd@execulink.ca>
2025-10-21 21:45:39 +0200

    Add test for auth fail, found HW spelling error needs fixing

--- sw/helloworld.c
+++ sw/helloworld.c
@@ -37,25 +37,35 @@ int test_dma_read( char *ptr, int max_len );
 int test_dma_write( char *ptr, int max_len, char *buf );
 volatile long *hw_reg = ((long *)0x20000000);
 
-// ascon test buffers
-char key[16] = { 0x90, 0xE4, 0x15, 0xD6, 0x42, 0xBF, 0xCD, 0x59, 0xF1, 0xFC, 0xCA, 0x19, 0x6B, 0x3B, 0xB3, 0x09 };
+/////////////////////////////////
+// ASCON test 
+////////////////////////////////
+// Test data from ascon-verilog test.py:14 changed to RUNS = (9,10) and run make sims
+// todo: Add NIST 800-232 defined tests
+
+// Fixed read only data, no change
+char  key[16] = { 0x90, 0xE4, 0x15, 0xD6, 0x42, 0xBF, 0xCD, 0x59, 0xF1, 0xFC, 0xCA, 0x19, 0x6B, 0x3B, 0xB3, 0x09 };
 char npub[16] = { 0x8C, 0xEE, 0x7C, 0xDD, 0x81, 0x83, 0xCA, 0x6A, 0xA2, 0xDC, 0x9B, 0x8B, 0x20, 0xA1, 0x6E, 0x8E };
-char ad[9] = { 0x2B, 0x0A, 0x5B, 0x7A, 0x81, 0xDE, 0x31, 0x73, 0xE2 };
-char pt[9] = { 0x32, 0x3B, 0x41, 0xEE, 0x00, 0xAE, 0x8A, 0x14, 0xAA };
-char opt[9] = { 0x32, 0x3B, 0x41, 0xEE, 0x00, 0xAE, 0x8A, 0x14, 0xAA };
-char ct[9] = { 0x3C, 0x71, 0xC7, 0xBA, 0xDE, 0x48, 0x01, 0x2E, 0x1D };
-char oct[9] = { 0x3C, 0x71, 0xC7, 0xBA, 0xDE, 0x48, 0x01, 0x2E, 0x1D };
-char otag[16] = { 0xB2, 0x6E, 0x66, 0xA8, 0xA5, 0x5D, 0x6A, 0x93, 0x28, 0xD8, 0xD0, 0x5B, 0xC1, 0x67, 0x8A, 0x3E };
-char tag[16];
-char auth[4];
-char hash[32];
-char ohash[32] = { 0xFB, 0xE3, 0x34, 0x4F, 0xE7, 0x91, 0xB5, 0x29, 0x89, 0xFD, 0x4C, 0x22, 0x05, 0x94, 0xDA, 0xAA, 
-                   0x72, 0x51, 0x6F, 0x55, 0x12, 0x2A, 0xBF, 0x75, 0xFC, 0x38, 0xAB, 0xF0, 0xA1, 0xBA, 0xB0, 0x75 };
-char oxof[16] = { 0xD6, 0x25, 0x86, 0x5F, 0x2F, 0x05, 0x4C, 0x7C, 0x32, 0xB7, 0x30, 0x42, 0xF5, 0x2B, 0xB8, 0x92 };
-char xof[16];
+char   ad[ 9] = { 0x2B, 0x0A, 0x5B, 0x7A, 0x81, 0xDE, 0x31, 0x73, 0xE2 };
 char ctsm[17] = { 0x48, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x90, 0xE4, 0x15, 0xD6, 0x42, 0xBF, 0xCD, 0x59, 0xF1 };
-char msg2[9] = { 0xFC, 0xCA, 0x19, 0x6B, 0x3B, 0xB3, 0x09, 0x8C, 0xEE };
-char ocxof[16] = { 0xC5, 0x6B, 0xE7, 0x6B, 0x23, 0xFF, 0x3B, 0x66, 0xE4, 0xE3, 0x79, 0x39, 0xF5, 0x0D, 0xFE, 0x0F };
+char msg2[ 9] = { 0xFC, 0xCA, 0x19, 0x6B, 0x3B, 0xB3, 0x09, 0x8C, 0xEE }; // for cxof input
+// Expected output data for comparison
+char  opt[9]  = { 0x32, 0x3B, 0x41, 0xEE, 0x00, 0xAE, 0x8A, 0x14, 0xAA }; 
+char  oct[9]  = { 0x3C, 0x71, 0xC7, 0xBA, 0xDE, 0x48, 0x01, 0x2E, 0x1D };
+char otag[16] = { 0xB2, 0x6E, 0x66, 0xA8, 0xA5, 0x5D, 0x6A, 0x93, 0x28, 0xD8, 0xD0, 0x5B, 0xC1, 0x67, 0x8A, 0x3E };
+char ohash[32]= { 0xFB, 0xE3, 0x34, 0x4F, 0xE7, 0x91, 0xB5, 0x29, 0x89, 0xFD, 0x4C, 0x22, 0x05, 0x94, 0xDA, 0xAA, 
+                  0x72, 0x51, 0x6F, 0x55, 0x12, 0x2A, 0xBF, 0x75, 0xFC, 0x38, 0xAB, 0xF0, 0xA1, 0xBA, 0xB0, 0x75 };
+char oxof[16] = { 0xD6, 0x25, 0x86, 0x5F, 0x2F, 0x05, 0x4C, 0x7C, 0x32, 0xB7, 0x30, 0x42, 0xF5, 0x2B, 0xB8, 0x92 };
+char ocxof[16]= { 0xC5, 0x6B, 0xE7, 0x6B, 0x23, 0xFF, 0x3B, 0x66, 0xE4, 0xE3, 0x79, 0x39, 0xF5, 0x0D, 0xFE, 0x0F };
+// Initial pt, ct data will be in-place overwritten but we have OG versions above for comparison
+char   pt[9]  = { 0x32, 0x3B, 0x41, 0xEE, 0x00, 0xAE, 0x8A, 0x14, 0xAA }; 
+char   ct[9]  = { 0x3C, 0x71, 0xC7, 0xBA, 0xDE, 0x48, 0x01, 0x2E, 0x1D }; 
+// Output buffers written by cipher unit
+char  tag[16];
+char auth[ 4];
+char fail[ 4];
+char hash[32];
+char  xof[16];
 char cxof[16];
 
 int main() {
@@ -66,17 +76,17 @@ int main() {
     // wait until uart has finished sending
     uart_write_flush();
 
-    // Create ASCON Instructions
+    /////////////////////////////////////////
+    // Build & Run Cipher Test Command List
+    /////////////////////////////////////////
     printf("ASCON test\n");
-    int  ad_len = 9;
-    int  msg_len = 9;
+    int  ad_len   = 9;
+    int  msg_len  = 9;
     int  hash_len = 16;
     int  ctsm_len = ad_len + 8;
 
-    //////////////////////////////////////
-    // Build Cipher Command List
-    //////////////////////////////////////
-    long cmd[30];
+    // Use a single command list to run the 5 cipher operations on the test data
+    long cmd[35];
     int  cidx = 0;
     cmd[cidx++] = (1/*ENC*/<<28) + (ad_len<<12) + (msg_len<<0); // { cmd[7:0], ad_len[11:0], msg_len[11:0] }
     cmd[cidx++] = (long)key;
@@ -84,6 +94,13 @@ int main() {
     cmd[cidx++] = (long)ad;
     cmd[cidx++] = (long)pt; // rw
     cmd[cidx++] = (long)tag; // w
+    cmd[cidx++] = (2/*DEC*/<<28) + (ad_len<<12) + (msg_len<<0); // { cmd[7:0], ad_len[11:0], msg_len[11:0] }
+    cmd[cidx++] = (long)key;
+    cmd[cidx++] = (long)npub;
+    cmd[cidx++] = (long)ad;
+    cmd[cidx++] = (long)ct; // r (no write if auth fails, any write will cause next test to fail)
+    cmd[cidx++] = (long)key; // != tag, will fail
+    cmd[cidx++] = (long)fail; // w
     cmd[cidx++] = (2/*DEC*/<<28) + (ad_len<<12) + (msg_len<<0); // { cmd[7:0], ad_len[11:0], msg_len[11:0] }
     cmd[cidx++] = (long)key;
     cmd[cidx++] = (long)npub;
@@ -100,7 +117,7 @@ int main() {
     cmd[cidx++] = (long)xof; // w
     cmd[cidx++] = (5/*CXOF*/<<28) + (ctsm_len<<12) + (msg_len<<0); // { cmd[7:0], ad_len[11:0], msg_len[11:0] }
     cmd[cidx++] = hash_len;
-    cmd[cidx++] = (long)ctsm; // read this as msg
+    cmd[cidx++] = (long)ctsm; // read this as context and msg len
     cmd[cidx++] = (long)msg2; // read this as msg
     cmd[cidx++] = (long)cxof; // w
 
@@ -110,22 +127,29 @@ int main() {
     // Execute Cipher Engine Command List
     //////////////////////////////////////
     //
-    hw_reg[4] = cidx<<2; // command list length
-    hw_reg[1] = (long)cmd; // Start execution on command list
-    while( (hw_reg[6] & 0x2011111) != 0x2011111 ); // wait for completion (dmas complete and cipher done)
+    long done = 0x2011111; // status flags indicating cipher and dma units are complete
+    hw_reg[4] = cidx<<2;   // command list length (as bytes
+    hw_reg[1] = (long)cmd; // Start execution on command list, write command list addr to the HW register
+    while( (hw_reg[6] & done) != done ); // poll for completion
     //
     //////////////////////////////////////
 
     printf("Done\n");
-    printf("Auth %x [", *(long *)auth );
-    for( int ii = 0; ii < 4; ii++ ) 
-	putchar( auth[ii] );
-    printf("]\n");
+    printf("Auth %x Fail %x [", *(long *)auth, (long *)fail );
+        for( int ii = 0; ii < 4; ii++ ) 
+		putchar( auth[ii] );
+        printf("] [");
+        for( int ii = 0; ii < 4; ii++ ) 
+		putchar( fail[ii] );
+        printf("]\n");
     	int err;
 	err = 0;
 	// AUth should be "pass"
         for( int ii = 0; ii < 4; ii++ ) 
 		if( auth[ii] != ((ii == 0)?'p':(ii==1)?'a':'s')) { err++; }
+	// fail should be "fail"
+        for( int ii = 0; ii < 4; ii++ ) 
+		if( fail[ii] != ((ii == 0)?'f':(ii==1)?'a':(ii==2)?'i':'l')) { err++; }
 	// pt should = oct
 	for( int ii = 0; ii < msg_len; ii++ )
 		if( pt[ii] != oct[ii] ) { err++; }
@@ -147,9 +171,11 @@ int main() {
 	printf( (err ) ? "\e[31mERROR\e[0m\n" : "\e[42mPASSED\e[0m\n");
 
     uart_write_flush();
+    ///////////// done ascon test /////////////////////////
     return(1);
 
-// uart loopback
+
+    // uart loopback
     uart_loopback_enable();
     printf("internal msg\n");
     sleep_ms(1);
